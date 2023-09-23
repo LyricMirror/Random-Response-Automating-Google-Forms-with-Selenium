55a27f3ce441a44b8052d65ee304d3dbd307c346
NirajSahu0412 <nirajsahu0412@gmail.com>
2023-09-23 17:55:44 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import auto_form_fill
 
-for x in range(10): # No of Responss you want to generate
+for x in range(10):  # No of Responss you want to generate
     auto_form_fill.form_fill()
     print((x + 1), " Response generated successfully")
